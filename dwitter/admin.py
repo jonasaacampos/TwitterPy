dfe70b6286959d7e9fcda381dea7eba92ec779ca
jaac <38704271+jonasaacampos@users.noreply.github.com>
2023-01-25 08:16:19 -0300

    finalização do projeto. Falta testar e documentar

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile, Dweet
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     # mostrar somente o campo username
     fields = ["username"]
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
@@ -14,4 +18,5 @@ admin.site.register(User, UserAdmin)
 
 # revmove a visualização dos grupos
 admin.site.unregister(Group)
-
+# admin.site.register(Profile)
+admin.site.register(Dweet)
--- dwitter/views.py
+++ dwitter/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile, Dweet
+from .forms import DweetForm
 
-# Create your views here.
+
+def dashboard(request):
+    if request.method == "POST":
+        form = DweetForm(request.POST)
+
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+
+    form = DweetForm()
+
+    followed_dweets = Dweet.objects.filter(
+        user__profile__in=request.user.profile.follows.all()
+    ).order_by("-created_at")
+
+    return render(request, "dashboard.html", {"form": form, "dweets": followed_dweets})
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    if not hasattr(request.user, "profile"):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
+    profile = Profile.objects.get(pk=pk)
+
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+
+    return render(request, "profile.html", {"profile": profile})
